2ae79d9711aa06e34d03179070ff0862434d5cbb
Mostafa <mostaf7583@gmail.com>
2024-06-17 00:58:00 +0300

    models

---
+++ server/models/transcationModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+    idClient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Client',
+        required: true
+    },
+    paid: {
+        type: Number,
+        required: true
+    },
+    amountOfCements: {
+        type: Number,
+        required: true
+    },
+    dateTime: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+module.exports = Transaction;
