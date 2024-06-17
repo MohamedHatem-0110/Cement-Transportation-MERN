da4420868f7da38945d7b26035455d6a054be1ee
Mohamed Hatem <83113802+MohamedHatem-0110@users.noreply.github.com>
2024-06-17 19:54:44 +0300

    initialize frontend client

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
